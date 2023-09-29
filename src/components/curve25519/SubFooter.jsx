4fffc85ccab0a3ee90718b42a4c8beddd342f531
redvelvetme <re@redvelvet.me>
2023-09-30 02:21:57 +0700

    add: curve25519 modulo

--- src/components/curve25519/SubFooter.jsx
+++ src/components/curve25519/SubFooter.jsx
@@ -3,9 +3,11 @@ import redvelvet_curve25519_public_key from '@/assets/png/redvelvet-curve25519-p
 function SubFooter() {
   return (
     <div className="sub-footer">
-      <label className="!mt-0">
+      <label className="!mt-0 !leading-5">
         <a href="https://www.desmos.com/calculator/4chrbl0xau" target="_blank" rel="noopener noreferrer nofollow">
           y&#178; = x&#xB3; + 486662x&#178; + x
+          <br />
+          modulo p = 2<sup>255</sup> - 19
         </a>
       </label>
       <div className="qr-code-curve25519-public">
