18b1a34b8a62bdfd89d64b9928447b2b649da0a9
redvelvetme <re@redvelvet.me>
2023-10-27 17:14:22 +0700

    update: contact info

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,12 @@ function Home({ t }) {
     <Container header={{ title: t('pages.home') }}>
       <p className="paragraph mt-4">
         {t('maintenance.p1')}&nbsp;
-        <a href="mailto:re@redvelvet.me">re@redvelvet.me</a>.<em className="text-gray-100">{t('maintenance.p2')}</em>
+        <a href="mailto:re@redvelvet.me">re@redvelvet.me</a> /{' '}
+        <a href="mailto:rasetiansyah@pm.me">rasetiansyah@pm.me</a> -{' '}
+        <a href="https://gitlab.com/monikaquentin.gpg" target="_blank" rel="noopener noreferrer nofollow">
+          OpenPGP
+        </a>
+        .<em className="text-gray-100">{t('maintenance.p2')}</em>
       </p>
       <hr className="border-t mt-4" />
       <PublicKeys />
